a8a6805894d108d6a5b40cb6c1efce987143bc18
spramani <shubhamramani903@gmail.com>
2020-12-22 16:45:12 +0530

    Initial Commit

---
+++ postAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  postAPI
+//
+//  Created by MAC on 22/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
